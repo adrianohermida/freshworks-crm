813cf07a37ab081c9a2c491bb3816dec372bbf6f
adrianohermida <adrianohermida@gmail.com>
2026-03-05 18:59:44 -0400

    4

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
@@ -14,13 +13,6 @@ export default defineConfig({
     emptyOutDir: true
   },
   plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
     react(),
   ]
 });
